887b3818a21c6a4d5d555793a427b1ef7bfd9d33
Noel J Abraham <60876099+noeljabraham@users.noreply.github.com>
2024-04-15 21:13:49 +0530

    scnd push

---
+++ index.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/call_contact1')
+def call_contact1():
+    phone_number = '9645951639'  # Phone number for Contact 1
+    return f'<script>window.location.href = "tel:{phone_number}";</script>'
+
+@app.route('/call_contact2')
+def call_contact2():
+    phone_number = '9447354934'  # Phone number for Contact 2
+    return f'<script>window.location.href = "tel:{phone_number}";</script>'
+
+if __name__ == '__main__':
+    app.run(debug=False,host='0.0.0.0')
