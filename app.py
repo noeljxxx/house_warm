68649566101b8751b11dbb3822e58834fb4a2abc
Noel J Abraham <60876099+noeljabraham@users.noreply.github.com>
2024-04-16 22:21:46 +0530

    tempatelll

--- app.py
+++ app.py
@@ -16,5 +16,4 @@ def call_contact2():
     phone_number = '9447354934'  # Phone number for Contact 2
     return f'<script>window.location.href = "tel:{phone_number}";</script>'
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
